90b8cfe5f2312d1430605ed1838edaa829c08ff7
Максим Рябов <maximorya@gmail.com>
2022-09-30 21:46:18 +0400

    spr_9_01

---
+++ yatube_api/api/serializers.py
@@ -0,0 +1,64 @@
+from rest_framework import serializers
+from rest_framework.fields import CurrentUserDefault
+from rest_framework.relations import SlugRelatedField
+from rest_framework.validators import UniqueTogetherValidator, UniqueValidator
+
+from posts.models import Comment, Follow, Group, Post, User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username')
+        ref_name = 'ReadOnlyUsers'
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = '__all__'
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = SlugRelatedField(slug_field='username', read_only=True)
+
+    class Meta:
+        fields = '__all__'
+        model = Post
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = SlugRelatedField(read_only=True,
+                              slug_field='username')
+    post = SlugRelatedField(read_only=True,
+                            slug_field='id')
+
+    class Meta:
+        fields = ('id', 'author', 'text', 'created', 'post')
+        model = Comment
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = SlugRelatedField(read_only=True,
+                            slug_field='username',
+                            default=CurrentUserDefault())
+
+    following = SlugRelatedField(slug_field='username',
+                                 queryset=User.objects.all(),
+                                 validators=[UniqueValidator(
+                                     queryset=User.objects.all())])
+
+    def validate_following(self, value):
+        user = self.context.get('request').user
+        if value == user:
+            raise serializers.ValidationError(
+                'Поля user и following не могут быть одинаковыми.')
+        return value
+
+    class Meta:
+        fields = ('user', 'following')
+        model = Follow
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=('user', 'following'))]
